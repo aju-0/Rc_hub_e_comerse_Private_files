c0e7f568c2ff412123f6e5bd7ec851b8c0901655
aju-0 <ajunair0@gmail.com>
2023-12-21 10:34:21 +0530

    corrected

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -92,7 +92,7 @@ const loadCheckOut = async (req, res) => {
 
             const totalamount = Total;
             
-            res.render("/checkOut", {
+            res.render("checkout", {
               userId: userId,
               products: products,
               total: Total,
